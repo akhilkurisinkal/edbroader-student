072b460b0f3761108bd160c02aa84b2723e05fff
akhilkurisinkal <akhilkthomas99@gmail.com>
2022-08-17 07:09:33 -0400

    ll

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 export const AppContext=createContext();
 export const AppProvider=(props)=>{
     const [pageName,setPageName]=useState("home");
-    const [isLogged,setIsLogged]=useState(true);
+    const [isLogged,setIsLogged]=useState();
 
     return(
         <AppContext.Provider value={{pageName,setPageName,isLogged,setIsLogged}}>
